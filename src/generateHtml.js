60b055fda2224d651d653f8bf8fa1025d8baf83d
koloseki <koloseki2002@gmail.com>
2023-04-25 13:48:00 +0200

    Added style.css , now website generate content dynamically

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -2,23 +2,31 @@ const html = document.documentElement;
 const body = document.body;
 const content = document.querySelector("#content");
 
-export function generateHTML() {
+
+export function generateMain() {
     content.innerHTML = "";
 
-    content.appendChild(document.createElement("div").appendChild(document.createElement("h2").appendChild(document.createTextNode("Hello World"))));
-    content.appendChild(document.createElement("div").appendChild(document.createElement("h3").appendChild(document.createTextNode("Hello World"))));
+    content.appendChild(document.createElement("div")).innerHTML = "<h2>Home</h2><p>Home is where the heart is</p>";
+
+
 }
 
 export function generateMenu(){
     content.innerHTML = "";
 
-    content.appendChild(document.createElement("div").appendChild(document.createElement("h2").appendChild(document.createTextNode("Menu"))));
-    content.appendChild(document.createElement("div").appendChild(document.createElement("h3").appendChild(document.createTextNode("Menu"))));
+    content.appendChild(document.createElement("div")).innerHTML = "<h2>Menu</h2><p>Menu is where the food is</p>";
+
+
+    content.appendChild(document.createElement('div')).innerHTML = "<h1><b>Margherita</b> <span class=\"w3-right w3-tag w3-dark-grey w3-round\">$12.50</span></h1>\n" +
+        "<p class=\"w3-text-grey\">Fresh tomatoes, fresh mozzarella, fresh basil</p>\n"
+
 }
 
 export function generateContacts(){
     content.innerHTML = "";
 
-    content.appendChild(document.createElement("div").appendChild(document.createElement("h2").appendChild(document.createTextNode("Contacts"))));
-    content.appendChild(document.createElement("div").appendChild(document.createElement("h3").appendChild(document.createTextNode("Contacts"))));
+    content.appendChild(document.createElement("div")).innerHTML = "<h2>Contacts</h2><p>Contacts is where the people are</p>";
+
+    content.appendChild(document.createElement("div")).innerHTML = "<iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d442.80118535455966!2d21.471497033565914!3d49.743553230808715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473db4735e0d70cb%3A0x4af53fef278d40c9!2sGlorietka!5e0!3m2!1spl!2spl!4v1682421632033!5m2!1spl!2spl\" width=\"600\" height=\"450\" style=\"border:0;\" allowfullscreen=\"\" loading=\"lazy\" referrerpolicy=\"no-referrer-when-downgrade\"></iframe>";
+
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import {generateContacts, generateHTML, generateMenu} from './generateHTML.js';
+import {generateContacts, generateMain, generateMenu} from './generateHTML.js';
+import './style.css';
+
 
 let home = document.querySelector("#hButton");
 let menu = document.querySelector("#mButton");
 let contacts = document.querySelector("#cButton");
 
-home.addEventListener('click', generateHTML);
+let content = document.querySelector("#content");
+
+content.classList.add('home');
+
+home.addEventListener('click', generateMain);
 
 menu.addEventListener('click', generateMenu);
 
 contacts.addEventListener('click', generateContacts);
+
+generateMain();
